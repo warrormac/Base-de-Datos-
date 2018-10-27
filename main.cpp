6a0b65c914c7f71dafc9ca07c622fc8fb1ce2e29
warrormac <andre.alvarez@ucsp.edu.pe>
2018-10-26 22:19:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <fstream>
@@ -14,44 +13,67 @@ using namespace std;
 int main()
 {
     string game;
-    int x=1;   //Este decide la opcion del menu 
+    int x=1;   //Este decide la opcion del menu
     int z=0;
     string *ptr;
     if (x==1)  //buscador de categorias
     {
         while (z==0)
         {
-            cout<<"Ingrese su categoria: ";
-            cin>>game;
-            ptr=&game;
 
-            juego nomb(ptr);
-
-            nomb.nombres();
-
-            z=nomb.buscar_juego();
-
-
-            if (z==0)
+            int x=0;
+            cout<< "presione 1 para ingresar alguna categoria, 0 para buscar en toda la base de datos.\n";
+            cin>>x;
+            while (x<0 && x>1)
             {
-                cout<<"Categoria invalida, intente de nuevo: \n";
-                system("pause");
+                cout<<"opcion invalida, intente denuevo.\n";
+                cout<<"presione 1 para ingresar alguna categoria, 0 para buscar en toda la base de datos.\n";
+                cin>>x;
+            }
+            if (x==1)
+            {
+                cout<<"Ingrese su categoria: ";
+                cin>>game;
+                ptr=&game;
+
+                juego nomb(ptr);
+                nomb.set_values(ptr);
+                nomb.nombres();
+
+                z=nomb.buscar_juego();
+
+
+                if (z==0)
+                {
+                    cout<<"Categoria invalida, intente de nuevo: \n";
+                    system("pause");
+                }
+                else
+                    nomb.busca();
             }
         }
-        buscador bus(ptr);
-        bus.busca();
+
         system("pause");
+
+
+
+
+
     }
 
 
-    if (x==2)   //buscador de juego   
+
+
+   /* if (x==2)   //buscador de juego
     {
         cout<<"ingrese el nombre del juego: ";
         cin>>game;
         ptr=&game;
-        
+
         editador busc(ptr);
 
-    }
+        busc buscar();
+
+    }*/
 
 }
