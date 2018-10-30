d0a151f31d00a738931df52f9642ff1c0edd44b7
warrormac <andre.alvarez@ucsp.edu.pe>
2018-10-29 19:49:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,22 +15,23 @@ int main()
     string game;
     int x=1;   //Este decide la opcion del menu
     int z=0;
+    int cat=0;
     string *ptr;
     if (x==1)  //buscador de categorias
     {
         while (z==0)
         {
 
-            int x=0;
+            int op=2;
             cout<< "presione 1 para ingresar alguna categoria, 0 para buscar en toda la base de datos.\n";
-            cin>>x;
-            while (x<0 && x>1)
+            cin>>op;
+            while (op<0 && op>1)
             {
-                cout<<"opcion invalida, intente denuevo.\n";
+                cout<<"opcion invalida, intente de nuevo.\n";
                 cout<<"presione 1 para ingresar alguna categoria, 0 para buscar en toda la base de datos.\n";
-                cin>>x;
+                cin>>op;
             }
-            if (x==1)
+            if (op==1)
             {
                 cout<<"Ingrese su categoria: ";
                 cin>>game;
@@ -40,40 +41,45 @@ int main()
                 nomb.set_values(ptr);
                 nomb.nombres();
 
-                z=nomb.buscar_juego();
+                cat=nomb.buscar_juego();
 
 
-                if (z==0)
+                if (cat==0)
                 {
                     cout<<"Categoria invalida, intente de nuevo: \n";
+
                     system("pause");
                 }
                 else
                     nomb.busca();
+                system("pause");
+
+
+            }
+            if (op==0)
+            {
+                cout<<"ingrese el nombre del juego: ";
+                cin>>game;
+                ptr=&game;
+
+                juego nom(ptr);
+                nom.set_values(ptr);
+                nom.nom_juego();
+
+                cat=nom.buscar_juego();
+
+                if (cat==0)
+                {
+                    cout<<"Juego no encontrado, intente de nuevo: \n";
+
+                    system("pause");
+                }
+                else
+                    nom.busca();
+                system("pause");
             }
         }
 
-        system("pause");
 
-
-
-
-
-    }
-
-
-
-
-   /* if (x==2)   //buscador de juego
-    {
-        cout<<"ingrese el nombre del juego: ";
-        cin>>game;
-        ptr=&game;
-
-        editador busc(ptr);
-
-        busc buscar();
-
-    }*/
-
+        }
 }
