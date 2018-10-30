ae6c907d4160f06fa69f0db5ac86adceeede9bd4
warrormac <andre.alvarez@ucsp.edu.pe>
2018-10-29 19:35:06 -0500

    Update juego.cpp

--- cpp/juego.cpp
+++ cpp/juego.cpp
@@ -24,15 +24,42 @@ void juego::nombres()
 
 
 }
+
+void juego::nom_juego()
+{
+    string nombre_juegos[30];
+    nombre_juegos[0]="age of empires";nombre_juegos[1]="assassins creed";
+    nombre_juegos[2]="broforce";nombre_juegos[3]="call of duty 4";
+    nombre_juegos[4]="call of duty mw2";nombre_juegos[5]="cs:go";
+    nombre_juegos[6]="dark souls";nombre_juegos[7]="diablo 3";
+    nombre_juegos[8]="dota";nombre_juegos[9]="fallout 76";
+    nombre_juegos[10]="FIFA 19";nombre_juegos[11]="firewatch";
+    nombre_juegos[12]="forza horizo 4";nombre_juegos[13]="GTA 5";
+    nombre_juegos[14]="halo";nombre_juegos[15]="heartstone";
+    nombre_juegos[16]="journey";nombre_juegos[17]="l.a noire";
+    nombre_juegos[18]="life is strange";nombre_juegos[19]="LOL";
+    nombre_juegos[20]="madden";nombre_juegos[21]="minecraft";
+    nombre_juegos[22]="nba 2k19";nombre_juegos[23]="overwatch";
+    nombre_juegos[24]="PUBG";nombre_juegos[25]="skyrim";
+    nombre_juegos[26]="strcraft";nombre_juegos[27]="the walking dead";
+    nombre_juegos[28]="undertale";nombre_juegos[29]="WoW";
+
+    this-> ptr=new string[30];
+    for (int y=0;y<30;y++)
+    {
+        ptr[y]=nombre_juegos[y];
+    }
+}
+
+
 int juego::buscar_juego()
 {
-        for (int y=0;y<7;y++)
+
+        for (int y=0;y<30;y++)
         {
             if (name==ptr[y])
                 return 1;
         }
         delete ptr;
         return 0;
-
-
 }
