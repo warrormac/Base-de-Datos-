3ac1173d57e8f47868cec610ad8f326a91a7395c
warrormac <andre.alvarez@ucsp.edu.pe>
2018-11-10 10:29:22 -0700

    Add files via upload

---
+++ h/ranking.h
@@ -0,0 +1,25 @@
+#include "buscador.h"
+#ifndef RANKING_H
+#define RANKING_H
+#include <string>
+
+using namespace std;
+
+template <class T>
+class ranking:public buscador
+{
+    public:
+        int equipo;
+        int xx;
+        string *ptr;
+
+
+    public:
+
+        ranking(int x);
+        T categoria();
+
+        T ranks(T valor);
+
+};
+#endif // RANKING_H
