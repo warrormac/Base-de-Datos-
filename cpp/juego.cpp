a0705b3147d688b8a7be71cfd323c575456d3546
warrormac <andre.alvarez@ucsp.edu.pe>
2018-11-13 18:13:34 -0500

    Update juego.cpp

--- cpp/juego.cpp
+++ cpp/juego.cpp
@@ -9,40 +9,50 @@ using namespace std;
 
 void juego::nombres()
 {
-        string categorias[8];
-        categorias[0]="Action";categorias[1]="Action-Adventure";
-        categorias[2]="Adventure";categorias[3]="Role-playing";
-        categorias[4]="Simulation";categorias[5]="Strategy";
-        categorias[6]="Sport";categorias[7]="Idle";
+        string categorias[7];
+        categorias[0]="Action & Adventure";categorias[1]="Fighting";
+        categorias[2]="Indie";categorias[3]="Roleplaying";
+        categorias[4]="RPG";categorias[5]="Shooters";
+        categorias[6]="Sports";
 
-        this -> ptr=new string[8];
-        for (int y=0;y<8;y++)
+        this -> ptr=new string[7];
+        for (int y=0;y<7;y++)
         {
             ptr[y]=categorias[y];
         }
-
-
 
 }
 
 void juego::nom_juego()
 {
-    string nombre_juegos[30];
-    nombre_juegos[0]="age of empires";nombre_juegos[1]="assassins creed";
-    nombre_juegos[2]="broforce";nombre_juegos[3]="call of duty 4";
-    nombre_juegos[4]="call of duty mw2";nombre_juegos[5]="cs:go";
-    nombre_juegos[6]="dark souls";nombre_juegos[7]="diablo 3";
-    nombre_juegos[8]="dota";nombre_juegos[9]="fallout 76";
-    nombre_juegos[10]="FIFA 19";nombre_juegos[11]="firewatch";
-    nombre_juegos[12]="forza horizo 4";nombre_juegos[13]="GTA 5";
-    nombre_juegos[14]="halo";nombre_juegos[15]="heartstone";
-    nombre_juegos[16]="journey";nombre_juegos[17]="l.a noire";
-    nombre_juegos[18]="life is strange";nombre_juegos[19]="LOL";
-    nombre_juegos[20]="madden";nombre_juegos[21]="minecraft";
-    nombre_juegos[22]="nba 2k19";nombre_juegos[23]="overwatch";
-    nombre_juegos[24]="PUBG";nombre_juegos[25]="skyrim";
-    nombre_juegos[26]="strcraft";nombre_juegos[27]="the walking dead";
-    nombre_juegos[28]="undertale";nombre_juegos[29]="WoW";
+    string nombre_juegos[52];
+    nombre_juegos[0]="Metro Exodus";nombre_juegos[1]="Halo 5";
+    nombre_juegos[2]="Battlefield 5";nombre_juegos[3]="Call of Duty Black Ops 4";
+    nombre_juegos[4]="PUBG";nombre_juegos[5]="Fortnite";
+    nombre_juegos[6]="Destiny 2";nombre_juegos[7]="Spyro Reignited Trilogy";
+    nombre_juegos[8]="HITMAN™ 2";nombre_juegos[9]="Minecraft";
+    nombre_juegos[10]="Red Dead Redemption 2";nombre_juegos[11]="Assassins Creed";
+    nombre_juegos[12]="Overwatch";nombre_juegos[13]="Destiny";
+    nombre_juegos[14]="Soulcalibur 6";nombre_juegos[15]="Street Fighter 30th Anniversary Collection";
+    nombre_juegos[16]="Injustice 2";nombre_juegos[17]="Dragon Ball Fighterz";
+    nombre_juegos[18]="Marvel VS Capcom: Infinite";nombre_juegos[19]="Teken 7";
+    nombre_juegos[20]="Mortal Kombat";nombre_juegos[21]="Killer Instinct";
+    nombre_juegos[22]="NARUTO SHIPPUDE: Ultimate Ninja STORM 4";nombre_juegos[23]="Warhammer: Vermintide 2";
+    nombre_juegos[24]="Next Up Hero";nombre_juegos[25]="Sky Force Reloaded";
+    nombre_juegos[26]="Layer of Fear";nombre_juegos[27]="Resident Evil 2";
+    nombre_juegos[28]="The Walking Dead: Season 1";nombre_juegos[29]="Tom Clancys The Division 2";
+    nombre_juegos[30]="Kindom Hearts 3";nombre_juegos[31]="Fallout 76";
+    nombre_juegos[32]="Darksiders 3";nombre_juegos[33]="Adventure Time: Pirates of the Enchiridion";
+    nombre_juegos[34]="State of Decay 2";nombre_juegos[35]="The Elder Scrolls V";
+    nombre_juegos[36]="The Witcher";nombre_juegos[37]="Dark Souls 3";
+    nombre_juegos[38]="Diablo 3";nombre_juegos[39]="Fallout 76";
+    nombre_juegos[40]="WoW"; nombre_juegos[41]="Final Fantasy 15";
+    nombre_juegos[42]="South Park: The Stick of Truth"; nombre_juegos[43]="NBA 2K";
+    nombre_juegos[44]="FIFA 19"; nombre_juegos[45]="WWE 2K";
+    nombre_juegos[46]="Rocket league"; nombre_juegos[47]="Madden NFL";
+    nombre_juegos[48]="Steep"; nombre_juegos[49]="Gran Turismo Sport";
+    nombre_juegos[50]="Tony Hawks Pro Skater";nombre_juegos[51]="Wii Sports";
+
 
     this-> ptr=new string[30];
     for (int y=0;y<30;y++)
@@ -57,6 +67,7 @@ int juego::buscar_juego()
 
         for (int y=0;y<30;y++)
         {
+
             if (name==ptr[y])
                 return 1;
         }
